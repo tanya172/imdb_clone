0584c4aebf49329f52a4926d884463a206db11af
tanya172 <tannyaverma086@gmail.com>
2023-02-07 18:33:47 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route index element={<h1>hello world</h1>}></Route>
+          <Route path='movie/:id' element={<h1>Movie detail page</h1>}></Route>
+          <Route path='movies/:type' element={<h1>Movie list page</h1>}></Route>
+          <Route path='/*' element={<h1>Error Page</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
