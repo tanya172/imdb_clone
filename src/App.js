e9e26ea02133501727a856062340995d7e038955
tanya172 <tannyaverma086@gmail.com>
2023-02-09 17:14:13 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ function App() {
       <Router>
         <Routes>
           <Route index element={<h1>hello world</h1>}></Route>
-          <Route path='movie/:id' element={<h1>Movie detail page</h1>}></Route>
-          <Route path='movies/:type' element={<h1>Movie list page</h1>}></Route>
-          <Route path='/*' element={<h1>Error Page</h1>}></Route>
+          <Route path="movie/:id" element={<h1>Movie detail page</h1>}></Route>
+          <Route path="movies/:type" element={<h1>Movie list page</h1>}></Route>
+          <Route path="/*" element={<h1>Error Page</h1>}></Route>
         </Routes>
       </Router>
+      <h1>heelo</h1>
     </div>
   );
 }
